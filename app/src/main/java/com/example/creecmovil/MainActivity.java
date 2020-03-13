ecc2b07ed669fce207376c54a3f777036d34e912
Juanelc197 <juanelc197@outlook.com>
2020-03-12 23:47:13 -0600

    modificando los botones del menu

--- app/src/main/java/com/example/creecmovil/MainActivity.java
+++ app/src/main/java/com/example/creecmovil/MainActivity.java
@@ -6,21 +6,39 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.io.StringReader;
+import java.net.URL;
 import java.time.Instant;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
     Button btn;
+    EditText txtemail, txtpass;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+       /* txtemail = (EditText) findViewById(R.id.txt_email);
+        txtpass = (EditText) findViewById(R.id.txt_pass);  */
+
         btn = (Button) findViewById(R.id.btn_prueba);
 
-        btn.setOnClickListener(new View.OnClickListener() {
+       btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
@@ -28,6 +46,37 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+
+       /* private void validarUsuario(String URL) {
+            StringRequest stringRequest = new StringRequest(Request.Method.POST, URL, new Response.Listener<String>() {
+                @Override
+                public void onResponse(String response) {
+                    if(!response.isEmpty()) {
+                        Intent intent = new Intent(getApplicationContext(), UserActivity.class);
+                        startActivity(intent);
+                    } else {
+                        Toast.makeText( MainActivity.this, "email o contraseña, incorrectos",Toast.LENGTH_SHORT).show();
+                    }
+
+                }
+            }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    Toast.makeText(MainActivity.this, error.toString(), Toast.LENGTH_SHORT).show();
+                }
+            }){
+                @Override
+                protected Map<String, String> getParams() throws AuthFailureError {
+                    Map<String, String> parametro = new HashMap<String, String>();
+                    parametro.put("email",txtemail.getText().toString());
+                    parametro.put("pass",txtpass.getText().toString());
+                    return super.getParams();
+                }
+            };
+
+            RequestQueue requestQueue= Volley.newRequestQueue(this);
+            requestQueue.add(stringRequest);
+        } */
 
     }
 
--- app/src/main/java/com/example/creecmovil/MenuActivity.java
+++ app/src/main/java/com/example/creecmovil/MenuActivity.java
@@ -5,11 +5,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 
 public class MenuActivity extends AppCompatActivity {
 
     ImageButton imgbuser, imgbsocial, imgbcontacto, imgbtransfer;
+
+    Button btnsocial, btntransf, btncont;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,7 +30,8 @@ public class MenuActivity extends AppCompatActivity {
             }
         });
 
-        imgbsocial = (ImageButton) findViewById(R.id.imgb_social);
+        //img btn social
+        /*imgbsocial = (ImageButton) findViewById(R.id.imgb_social);
 
         imgbsocial.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -36,7 +40,20 @@ public class MenuActivity extends AppCompatActivity {
                 startActivity(new Intent(MenuActivity.this, SocialActivity.class));
 
             }
+        }); */
+
+        btnsocial = (Button) findViewById(R.id.btn_social);
+
+        btnsocial.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                startActivity(new Intent(MenuActivity.this, SocialActivity.class));
+
+            }
         });
+        // img btn contacto
+        /*
 
         imgbcontacto = (ImageButton) findViewById(R.id.imgb_contacto);
 
@@ -47,11 +64,42 @@ public class MenuActivity extends AppCompatActivity {
                 startActivity(new Intent(MenuActivity.this, ActivityContacto.class));
 
             }
+        }); */
+
+        btncont = (Button) findViewById(R.id.btn_contacto);
+
+        btncont.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                startActivity(new Intent(MenuActivity.this, ActivityContacto.class));
+
+            }
         });
+
+        //img btn transf
+         /*
 
         imgbtransfer = (ImageButton) findViewById(R.id.imgb_transfer);
 
+
+
         imgbtransfer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                startActivity(new Intent(MenuActivity.this, TransferActivity.class));
+
+            }
+        });
+
+         */
+
+        btntransf = (Button) findViewById(R.id.btn_transf);
+
+
+
+        btntransf.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
